a13410f62890df76762f39f8ff7c75b473acf398
Prity Patel <pritypatel714@gmail.com>
2015-07-07 15:55:40 -0700

    majority of html and css styling complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 
   console.log('activate framework!');
 
+// Pseudocode?
+// When player lands on page, load page as quickly as possible
+      // do this using an Event Listener
+
+// Reveal card image once player clicks on card.
+
+// Allow player to click on a max of 2 cards at a time.
+      // Use a 'while loop'?
+
+// Reveal card images of a max of 2 cards at a time.
+// Player gains 1 entry for every 2 cards clicked.
+// Player can keep playing until all card images have been matched
+// Number of entries should be displayed.
+// If 2 consecutively clicked cards match, leave those 2 card images revealed and do not allow those cards to be clicked again.
+// If 2 consecutively clicked cards do not match, hide revealed card images allow those cards to be clicked again.
+// Once all 12 card images have been revealed and match, alert player with message of final entries.
+//
+
