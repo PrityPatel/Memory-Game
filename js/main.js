b9ad9f5cd32346b5dd0d9f50159a71cd4b2a6949
Prity Patel <pritypatel714@gmail.com>
2015-07-09 19:50:14 -0700

    cleaned up the code and comments on the js file; next step writing the readme file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-console.log('activate framework!')
+console.log("ISSUE #1: NEED TO WRITE FUNCTION SO THAT THE MATCHED ITEMS ARE NOT RECLICKABLE");
+console.log("ISSUE #2: NEED TO WRITE FUNCTION TO RESET BOARD AFTER ALL 6 MATCHES ARE MADE");
 
 //**********************************
 //the following is the function to load the page using the jQuery first; "wait until DOM has finished loading and the load 'this' code."
@@ -14,75 +15,68 @@ console.log('activate framework!')
 //using while loop until all indexes are set and then returns the new array.
 
 var arr = ["chinesefortune", "cloverfortune", "catfortune", "horseshoefortune", "lovefortune", "goldpotfortune", "chinesefortune", "cloverfortune", "catfortune", "horseshoefortune", "lovefortune", "goldpotfortune"];
+//setting variable to include all class names (images) to be added from the array into the square
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   //and then return a new array
   return array;
 }
-//call the function so the images are shuffled upon refresh
+//then call the shuffle function so the images are shuffled upon refresh
 shuffle(arr);
 
+///////VARIABLES///////
 
 var $squares =$(".square");
 // console.log($squares);
 
 var checkMatch= [];
-//setting variable to write functions for matching cards
+//setting variable to be used in a function to see if cards match.
 
-var clickCounter= 0;
-//setting variable to write functions to maintain 2 clicks per turn
+var matchAttempt= 0;
+//setting variable to keep track of how many clicks were made to attempt at a match. this resets at every 2nd click as it takes 2 attempts to see if a match is made.
 
-var matches= 0;
-//setting variable to write a function to make sure that once all matches are made, then the amount of attempts made to make matches is sent as an alert to the player in the form of a Fortune Number.
+var totalMatches= 0;
+//setting variable to keep track of the total amount of matches and to make sure that once all 6 matches are made, an altert is sent to the player with their individual Fortune Number.
 
-var attemptsMade= 0;
-//setting variable to keep track of how many attempts are made to make matches. The Fortune number is the amount of the amount of attempts made times 2 as each click counts towards the Fortun
+var totalClicks= 0;
+//setting variable to keep track of how many total clicks were made on the baord as this number will be the Fortune Number that is alerted to the player once all 6 matches are made.
 
-// var divArray= [];
-
-//the below is i realize adds the respective image class onto to the board, i'm just not sure how
+//the below i realize adds the respective image class onto to the board, i'm not sure exactly how.
+//putting console log elements into long form english, so that it easy for my 'new to coding' brain make sense of what is appearing.
 $squares.on("click", function() {
-  if (clickCounter < 2) {
+  if (matchAttempt < 2) {
   $(this).addClass(arr[parseInt(this.id)]);
   checkMatch.push(arr[parseInt(this.id)]);
-  clickCounter++;
-  attemptsMade++;
-  console.log("name of object in the array: " + arr[parseInt(this.id)]);
+  matchAttempt++;
+  totalClicks++;
   console.log("checking the match of these objects in the array: " + checkMatch);
-  console.log("how many clicks: " + clickCounter);
-  console.log("attempts made: " + attemptsMade);
-  console.log("matches: " + matches);
+  console.log("total clicks thus far: " + totalClicks);
+  console.log("how many matches thus far: " + totalMatches);
   clickMatch();
 }
 });
 
-
 function clickMatch() {
-  if (clickCounter === 2) {
-      console.log("value " + checkMatch[0], checkMatch[1]);
+  if (matchAttempt === 2) {
     if (checkMatch[0] === checkMatch[1]) {
-      console.log("Good Job. Keep Going!");
-      //console.log("Keep Going. Good Fortune Is Coming Your Way!");
       checkMatch= [];
-      matches++;
+      totalMatches++;
       matchesMade();
-      console.log("matches: " + matches);
-      clickCounter = 0;
+      console.log("how many matches thus far: " + totalMatches);
+      console.log("Match Made!")
+      matchAttempt = 0;
       return;
     } else {
       reHide();
@@ -90,34 +84,24 @@ function clickMatch() {
   }
 }
 
+function reHide() {
+  setTimeout(function() {
+    $("." + checkMatch[0]).removeClass(checkMatch[0]);
+    $("." + checkMatch[1]).removeClass(checkMatch[1]);
+    checkMatch = [];
+    matchAttempt = 0;
+  alert("Keep Calm and Focus. Your Fortune Is Tied To Your Focus!");
+  }, 900);
+}
+
 function matchesMade() {
-  if (matches === 6) {
-    alert("Your Fortune Has Arrived. Your Fortune Number is: " + attemptsMade);
+  if (totalMatches === 6) {
+    alert("Your Fortune Has Arrived. Your Fortune Number is: " + totalClicks);
   } else {
     return;
   }
 }
 
-function reHide() {
-  console.log("Try Again.");
-  setTimeout(function() {
-    $("." + checkMatch[0]).removeClass(checkMatch[0]);
-    $("." + checkMatch[1]).removeClass(checkMatch[1]);
-    checkMatch = [];
-    clickCounter = 0;
-  }, 1100);
-}
-
-
 //ISSUES:
 //NEED TO WRITE FUNCTION SO THAT THE MATCHED ITEMS ARE NOT RECLICKABLE
-// If 2 consecutively clicked cards match, leave those 2 card images revealed and do not allow those cards to be clicked again.
-// If cards match then keep revealed and allow player to click an additional 2 cards
-
-//NEED TO WRITE FUNCTION TO STOP ABILITY TO CLICK ON THE BOARD AGAIN AFTER 6 MATCHES
-// Player can keep playing until all card images have been matched
-
-// When player lands on page, load page as quickly as possible
-      // do this using an Event Listener? -us jqery library load function - notated above; wrap everything withing jquery function
-
-
+//NEED TO WRITE FUNCTION TO RESET BOARD AFTER ALL 6 MATCHES ARE MADE
